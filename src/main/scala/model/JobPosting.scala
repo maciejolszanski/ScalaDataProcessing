a15681df7673884c0eca0b034bc68aa79edce1f3
Maciej Olszański <olszanskimaciej@wp.pl>
2025-05-29 18:54:02 +0200

    add scalafmt for intellij Idea

--- src/main/scala/model/JobPosting.scala
+++ src/main/scala/model/JobPosting.scala
@@ -2,5 +2,14 @@ package model
 
 import java.time.LocalDateTime
 
-case class JobPosting(sourceURL: String, title:String, company:String, description: String, salaryRange: String, skills: List[String], location: String, isRemote: Boolean, datePublished: LocalDateTime) {
-}
+case class JobPosting(
+    sourceURL: String,
+    title: String,
+    company: String,
+    description: String,
+    salaryRange: String,
+    skills: List[String],
+    location: String,
+    isRemote: Boolean,
+    datePublished: LocalDateTime
+)
