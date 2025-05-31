e965128422d2fe56ecdbd8abeccb4883eeafbb73
Maciej Olszański <olszanskimaciej@wp.pl>
2025-05-31 13:59:08 +0200

    add parsing CSV into JobPosting list

--- src/main/scala/util/Utils.scala
+++ src/main/scala/util/Utils.scala
@@ -1,15 +1,52 @@
 package util
 
+import model.JobPosting
 import scala.io.Source
+import java.time.LocalDateTime
 
 object Utils {
-  def readCSV(path: String): List[String] = {
+  def readCSV(path: String, header: Boolean): List[String] = {
     val file = Source.fromFile(path)
 
     try {
-      file.getLines().toList
+      val allLines = file.getLines().toList
+      val lines = if header then allLines.tail else allLines
+      lines
     } finally {
       file.close()
+    }
+  }
+
+  def parseJobPostings(
+      postingsText: List[String],
+      separator: Char
+  ): List[JobPosting] = {
+    postingsText.map(_.split(separator).createJobPosting)
+  }
+
+  extension (postingElems: Array[String]) {
+    private def createJobPosting: JobPosting = {
+      val sourceURL     = postingElems(0)
+      val title         = postingElems(1)
+      val company       = postingElems(2)
+      val description   = postingElems(3)
+      val salaryRange   = postingElems(4)
+      val skills        = postingElems(5).split(',').toList
+      val location      = postingElems(6)
+      val isRemote      = postingElems(7).toBoolean
+      val datePublished = LocalDateTime.parse(postingElems(8))
+
+      JobPosting(
+        sourceURL,
+        title,
+        company,
+        description,
+        salaryRange,
+        skills,
+        location,
+        isRemote,
+        datePublished
+      )
     }
   }
 }
--- src/test/scala/UtilsSpec.scala
+++ src/test/scala/UtilsSpec.scala
@@ -1,10 +1,23 @@
+import model.JobPosting
 import org.scalatest.funsuite.AnyFunSuite
+
+import java.time.LocalDateTime
 import java.io.FileNotFoundException
 import util.Utils
 
 class UtilsSpec extends AnyFunSuite {
-  test("test should read a CSV file") {
-    val file_content = Utils.readCSV("src/test/resources/test.csv")
+  test("test should read a CSV file omitting header when header=true") {
+    val file_content = Utils.readCSV("src/test/resources/test.csv", true)
+
+    val expected_content = List(
+      "value1,value2",
+      "value3,value4"
+    )
+
+    assert(file_content == expected_content)
+  }
+  test("test should read a CSV file when header=false") {
+    val file_content = Utils.readCSV("src/test/resources/test.csv", false)
 
     val expected_content = List(
       "columnName1,columnName2",
@@ -17,7 +30,20 @@ class UtilsSpec extends AnyFunSuite {
 
   test("test incorrect path") {
     intercept[FileNotFoundException] {
-      Utils.readCSV("non/existing/file.csv")
+      Utils.readCSV("non/existing/file.csv", true)
     }
+  }
+
+  test("test parsing JobPosting from list of elems") {
+    val input = List(
+      "https://jobs.example.com/job/1;Data Engineer;DataCorp;Responsible for building data pipelines.;15000-20000;Python,SQL,Scala;Warszawa, Polska;false;2025-05-10T09:00:00",
+      "https://jobs.example.com/job/2;Backend Developer;TechSolutions;Develop backend services.;14000-18000;Java,Kotlin,Spring;Kraków, Polska;true;2025-05-14T10:30:00"
+    )
+    val expectedOutput = List(
+      JobPosting("https://jobs.example.com/job/1", "Data Engineer", "DataCorp", "Responsible for building data pipelines.", "15000-20000", List("Python", "SQL", "Scala"), "Warszawa, Polska", false, LocalDateTime.of(2025, 5, 10, 9, 0, 0)),
+      JobPosting("https://jobs.example.com/job/2", "Backend Developer", "TechSolutions", "Develop backend services.", "14000-18000", List("Java", "Kotlin", "Spring"), "Kraków, Polska", true, LocalDateTime.of(2025, 5, 14, 10, 30, 0))
+    )
+
+    val actualOutput = Utils.parseJobPostings(input, ';')
   }
 }
