d05a264a944577ac37b83a3e065bc6ea9e1af483
Maciej Olszański <olszanskimaciej@wp.pl>
2025-05-31 16:35:29 +0200

    add main code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,9 @@
+import util.Utils
+
 object Main {
   def main(args: Array[String]): Unit = {
-    
+    val path        = "data/job_postings_structured.csv"
+    val fileContent = Utils.readCSV(path, header = true)
+    val jobPostings = Utils.parseJobPostings(fileContent, separator = ';')
   }
 }
